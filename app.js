24bfc682162cf48794e1fefa1a07a3ff3088a863
Guadalupe <guadalupe04.lc@gmail.com>
2023-09-08 14:24:54 -0600

    01

---
+++ app.js
@@ -0,0 +1,4 @@
+function hola(){
+console.log("hola desde el navegador");
+alert("hola desde el navegador")
+}
---
+++ tipos.js
@@ -0,0 +1,32 @@
+console.log("hola desde node")
+
+//comentario de una linea
+/*
+    para comentarios de bloque
+*/
+
+//diferentes tipos de datos 
+/*
+number
+string
+boolean
+object
+undefined
+*/
+
+console.log(123)
+console.log(typeof(123))
+
+console.log(2.3)
+console.log(typeof(2.3))
+
+console.log(true)
+console.log(typeof(true))
+
+console.log("a")
+console.log(typeof("a"))
+
+console.log([])
+console.log(typeof(true))
+
+
