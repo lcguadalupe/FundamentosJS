9a5fb358d3fed4528736518267f94b8a9cb7f3f1
Guadalupe <guadalupe04.lc@gmail.com>
2023-09-19 12:00:05 -0600

    09 Ciclos

---
+++ ciclos.js
@@ -0,0 +1,41 @@
+// Ciclos en javascript 
+
+// for 
+for(let i = 0; i<3; i++){
+    console.log('vuelta'+i);
+}
+// while 
+let contador = 0;
+while(contador < 3){
+    console.log('vuelta' + contador);
+
+    contador++;
+}   
+
+// do while 
+contador = 0;
+do{
+    console.log('vuelta' + contador);
+    contador++;
+} while(contador === 0);
+
+// recorrido de un string
+const name = 'peter';
+for (let i = 0; i<name.length; i++){
+    console.log(name[1]);
+}
+
+// recorrido de un array
+const numbers = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i<numbers.length; i++){
+    console.log(numbers[1]);
+}
+
+// for ... of 
+for (const item of numbers){
+    console.log(item);
+}
+
+for (const letter of name){
+    console.log(letter);
+}
