e8b825a99b26d28a8d1748ddc907f0fe42897265
Guadalupe <guadalupe04.lc@gmail.com>
2023-09-19 11:37:21 -0600

    08 arrays

---
+++ arrays.js
@@ -0,0 +1,33 @@
+// Arrays en javascript
+const miArray = [1, 2, 4, true, 'Hello', 'A', 2.1, [1, 1]]
+console.log(miArray);
+console.log(miArray[0]);
+console.log(miArray[1]);
+console.log(miArray[-1]); // Verificar 
+miArray[0] = "One";
+miArray[3, false];
+console.log(miArray);
+
+
+const pila = [];
+// Inserta con push
+pila.push(1);
+pila.push(2);
+pila.push(3);
+console.log(pila)
+console.log(pila.pop())
+console.log(pila)
+console.log(pila.pop())
+console.log(pila)
+
+//Colas en JS
+// first in first out
+const cola = []
+cola.unshift(1);
+cola.unshift(2);
+cola.unshift(3);
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
